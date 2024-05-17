eb17e58cc68a6c8222ad77f5323e1d89779be2fa
Crisper <102473956+crisper2001@users.noreply.github.com>
2024-05-17 19:08:50 -0300

    IDs corrigidos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import {Routes} from '@angular/router';
 import {FootballersListComponent} from "./footballers/footballers-list/footballers-list.component";
 import {FootballerResolve} from "./footballers/shared/footballer.resolve";
+import {FootballersFormComponent} from "./footballers/footballers-form/footballers-form.component";
 
 export const routes: Routes = [
   {
     path: '', component: FootballersListComponent,
     resolve: {footballerData: FootballerResolve}
+  },
+  {
+    path: 'new', component: FootballersFormComponent
   }
 ];
